e0fbc73c9c71f6a1281eb7ce68813c4d60384989
Luiz Carlos <luizgadao@gmail.com>
2019-02-11 23:50:54 -0200

    show maior e menor lance

--- app/src/main/java/br/com/alura/leilao/model/Leilao.java
+++ app/src/main/java/br/com/alura/leilao/model/Leilao.java
@@ -8,14 +8,36 @@ public class Leilao implements Serializable {
 
     private final String descricao;
     private final List<Lance> lances;
+    private double maiorLance = Double.NEGATIVE_INFINITY;
+    private double menorLance = Double.POSITIVE_INFINITY;
 
     public Leilao(String descricao) {
         this.descricao = descricao;
         this.lances = new ArrayList<>();
     }
 
+    public void add(Lance lance) {
+        lances.add(lance);
+
+        double valor = lance.getValor();
+        if (valor > maiorLance) {
+            maiorLance = valor;
+        }
+
+        if (valor < menorLance) {
+            menorLance = valor;
+        }
+    }
+
     public String getDescricao() {
         return descricao;
     }
 
+    public double getMaiorLance() {
+        return maiorLance;
+    }
+
+    public double getMenorLance() {
+        return menorLance;
+    }
 }
--- app/src/main/java/br/com/alura/leilao/ui/activity/LancesLeilaoActivity.java
+++ app/src/main/java/br/com/alura/leilao/ui/activity/LancesLeilaoActivity.java
@@ -1,11 +1,9 @@
 package br.com.alura.leilao.ui.activity;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
-
-import java.io.Serializable;
 
 import br.com.alura.leilao.R;
 import br.com.alura.leilao.model.Leilao;
@@ -21,6 +19,12 @@ public class LancesLeilaoActivity extends AppCompatActivity {
             Leilao leilao = (Leilao) dadosRecebidos.getSerializableExtra("leilao");
             TextView descricao = findViewById(R.id.lances_leilao_descricao);
             descricao.setText(leilao.getDescricao());
+
+            TextView maiorLance = findViewById(R.id.lances_leilao_maior_lance);
+            maiorLance.setText(String.valueOf(leilao.getMaiorLance()));
+
+            TextView menorLance = findViewById(R.id.lances_leilao_menor_lance);
+            menorLance.setText(String.valueOf(leilao.getMenorLance()));
         }
     }
 }
